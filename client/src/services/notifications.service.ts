c00ecc189c966acd7da9d683961e26a305bb0125
zennajjames <zennajjames@gmail.com>
2026-02-11 13:25:41 -0600

    fix: normalize REACT_APP_API_URL to not include /api

--- client/src/services/notifications.service.ts
+++ client/src/services/notifications.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const API_URL = `${process.env.REACT_APP_API_URL || 'http://localhost:8000'}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
